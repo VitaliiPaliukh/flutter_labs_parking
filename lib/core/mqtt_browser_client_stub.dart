f85aad93ad455d7412a13cf75c9c4667e3ba643e
VitaliiPaliukh <vitalikpaliukh@gmail.com>
2026-05-07 22:20:29 +0300

    fix

--- lib/core/mqtt_browser_client_stub.dart
+++ lib/core/mqtt_browser_client_stub.dart
@@ -2,8 +2,8 @@ import 'package:mqtt_client/mqtt_server_client.dart';
 
 class MqttBrowserClient extends MqttServerClient {
   MqttBrowserClient.withPort(
-    String server,
-    String clientIdentifier,
-    int port,
-  ) : super.withPort(server, clientIdentifier, port);
+    super.server,
+    super.clientIdentifier,
+    super.port,
+  ) : super.withPort();
 }
